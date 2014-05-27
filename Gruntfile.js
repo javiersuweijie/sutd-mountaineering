664474640a783169e16720853e5cbd8772d998f4
Javier <javier.su.weijie@gmail.com>
2014-05-27 16:54:44 +0800

    Changed some configs to include .js files that are inside folders

--- Gruntfile.js
+++ Gruntfile.js
@@ -178,6 +178,7 @@ module.exports = function ( grunt ) {
           '<%= vendor_files.js %>', 
           'module.prefix', 
           '<%= build_dir %>/src/**/*.js', 
+          '<%= build_dir %>/src/**/**/*.js', 
           '<%= html2js.app.dest %>', 
           '<%= html2js.common.dest %>', 
           'module.suffix' 
@@ -370,6 +371,7 @@ module.exports = function ( grunt ) {
         src: [
           '<%= vendor_files.js %>',
           '<%= build_dir %>/src/**/*.js',
+          '<%= build_dir %>/src/**/**/*.js',
           '<%= html2js.common.dest %>',
           '<%= html2js.app.dest %>',
           '<%= vendor_files.css %>',
--- build.config.js
+++ build.config.js
@@ -62,6 +62,7 @@ module.exports = {
    */
   vendor_files: {
     js: [
+      'vendor/jquery/dist/jquery.min.js',
       'vendor/angular/angular.js',
       'vendor/angular-bootstrap/ui-bootstrap-tpls.min.js',
       'vendor/placeholders/angular-placeholders-0.0.1-SNAPSHOT.min.js',
